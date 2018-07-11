e7393ebe7b6953b90934bbdd4bcb71c74dcd9174
Derek Bruun <derekbruun@gmail.com>
2018-07-10 23:31:05 -0400

    Initial push

---
+++ MetalBot/Modules/Ping.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetalBot.Modules
+{
+    public class Ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("Let me sleep");
+        }
+
+        [Command("We built this city")]
+        public async Task TestAsync()
+        {
+            await ReplyAsync("on rock and roll");
+        }
+    }
+}
---
+++ MetalBot/Program.cs
@@ -0,0 +1,88 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MetalBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+       
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _service;
+        
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _service = new ServiceCollection().AddSingleton(_client).AddSingleton(_commands).BuildServiceProvider();
+
+            String BotToken = "NDY2Mjk4ODczMDg2NzM4NDMz.DiaDZg.lcyoY29EW5_feuRL7xHgV-ZVcbM";
+
+            //event subscriptions
+            _client.Log += Log;
+
+            _client.UserJoined += AnnounceUserJoined;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, BotToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task AnnounceUserJoined(SocketGuildUser user)
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.AddField("Welcome!", $"Welcome to the NixxyPlays discord {user.Mention}");
+            builder.AddInlineField("Rules", "Please take a look at the rules in #rules");
+            builder.AddInlineField("Questions?", "Feel free to message a mod or the admin if you have any questions");
+            var guild = user.Guild;
+            var channel = guild.DefaultChannel;
+          //  guild.
+            await channel.SendMessageAsync($"Welcome {user.Mention}", false, builder);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("met!", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+               var result = await _commands.ExecuteAsync(context, argPos, _service);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
+    }
+}
